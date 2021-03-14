f8b99e74f1e29872b72374d73b87e2069c3604d3
Felippe Neves <felippenevesmonteiro@gmail.com>
2021-03-14 01:34:52 -0300

    OK

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
-import { SafeAreaView, Text, ImageBackground, StyleSheet, View, TextInput, TouchableOpacity, Platform } from 'react-native'
+import { Text, ImageBackground, StyleSheet, View, TouchableOpacity, Alert } from 'react-native'
 
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
+import AuthInput from '../components/AuthInput'
 
 const initialState = {
+    name: '',
     email: '',
-    password: ''
+    password: '',
+    confirmPassword: '',
+    stageNew: false
 }
 
 export default class Auth extends Component {
@@ -15,24 +19,56 @@ export default class Auth extends Component {
         ...initialState
     }
 
+    siginOrSignup = () => {
+        if(this.state.stageNew) {
+            Alert.alert('Sucesso!', 'Criar a conta')
+        } else {
+            Alert.alert('Sucesso!', 'Logar')
+        }
+    }
+
     render() {
         return(
             <ImageBackground style = {styles.background}
                 source = {backgroundImage}>
                 <Text style = {styles.title}>Tasks</Text>
                 <View style = {styles.formContainer}>
-                    <TextInput placeholder = 'E-mail' value = {this.state.email}
+                    <Text style = {styles.subtitle}>
+                        {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados'}
+                    </Text>
+                    {
+                        this.state.stageNew &&  <AuthInput icon = 'user' placeholder = 'Nome' 
+                                                    value = {this.state.name}
+                                                    style = {styles.input} 
+                                                    onChangeText = {name => this.setState({ name })}/>
+                    }
+                    <AuthInput icon = 'at' placeholder = 'E-mail' 
+                        value = {this.state.email}
                         style = {styles.input} 
                         onChangeText = {email => this.setState({ email })}/>
-                    <TextInput placeholder = 'Senha' value = {this.state.password}
-                        style = {styles.input} 
+                    <AuthInput icon = 'lock' placeholder = 'Senha' value = {this.state.password}
+                        style = {styles.input} secureTextEntry = {true}
                         onChangeText = {password => this.setState({ password })}/>
-                    <TouchableOpacity>
+                    {
+                        this.state.stageNew &&  <AuthInput icon = 'asterisk' 
+                                                    placeholder = 'Confirmação de Senha' value = {this.state.confirmPassword}
+                                                    style = {styles.input} secureTextEntry = {true}
+                                                    onChangeText = {confirmPassword => this.setState({ confirmPassword })}/>
+                    }
+                    <TouchableOpacity onPress = {this.siginOrSignup}>
                         <View style = {styles.button}>
-                            <Text style = {styles.buttonText}>Entrar</Text>
+                            <Text style = {styles.buttonText}>
+                                {this.state.stageNew ? 'Registrar' : 'Entrar'}
+                            </Text>
                         </View>
                     </TouchableOpacity>
                 </View>
+                <TouchableOpacity style = {{ padding: 10 }}
+                    onPress = {() => {this.setState({ stageNew: !this.state.stageNew })}}>
+                    <Text style = {styles.buttonText}>
+                        {this.state.stageNew ? 'Já possui conta?' : 'Ainda não possui conta?'}
+                    </Text>
+                </TouchableOpacity>
             </ImageBackground>
         )
     }
@@ -51,6 +87,13 @@ const styles = StyleSheet.create({
         fontSize: 70,
         marginBottom: 10
     },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20,
+        textAlign: 'center',
+        marginBottom: 10
+    },
     formContainer: {
         backgroundColor: 'rgba(0, 0, 0, 0.8)',
         padding: 20,
@@ -59,17 +102,17 @@ const styles = StyleSheet.create({
     input: {
         marginTop: 10,
         backgroundColor: '#FFF',
-        padding: Platform.OS === 'ios' ? 15 : 10
     },
     button: {
         backgroundColor: '#080',
         marginTop: 10,
         padding: 10,
-        alignItems: 'center'
+        alignItems: 'center',
+        borderRadius: 8
     }, 
     buttonText: {
         fontFamily: commonStyles.fontFamily,
         color: '#FFF',
         fontSize: 20
-    }
+    },
 })
